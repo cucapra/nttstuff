aee71d378e6f93a8b86be87d3e90644cda08747f
cgyurgyik <gyurgyikcp@gmail.com>
2021-01-17 12:27:18 -0500

    Factor out nttystuff

--- nttstuff.py
+++ nttstuff.py
@@ -1,18 +1,9 @@
 from sympy.discrete.transforms import ntt
-from sympy.ntheory import isprime, primitive_root
 import random
 import math
 import json
 import argparse
-
-
-def find_prime(n):
-    """Pick a prime number k*n+1 for some k."""
-    for k in range(1, 100):
-        p = k * n + 1
-        if isprime(p):
-            return p
-    assert False, 'prime not found!!!1111one'
+from ntt_utils import check_eq, _data, find_prime, reverse_bits, gen_omegas
 
 
 def naive_ntt(inp, P, omegas):
@@ -53,43 +44,6 @@ def cooley_tukey_ntt(inp, P, omegas):
         M = M * 2
 
     return ret
-
-
-def _data(arr, bitwidth=32):
-    """Make a FuTIL-ready JSON data dict."""
-    return {'data': arr, 'bitwidth': bitwidth}
-
-
-def check_eq(a, b):
-    """Assert that two arrays are equal everywhere."""
-    for i, (x, y) in enumerate(zip(a, b)):
-        assert x == y, f'difference at element {i}: {x}, {y}'
-    print('ok!')
-
-
-def gen_omegas(n, p):
-    # Generate an omega: g^k (mod p) for a generator of the field, g.
-    g = primitive_root(p)
-    k = (p - 1) // n
-    omega = (g ** k) % p
-
-    # Generate pre-computed omega array (also from Shunning).
-    omegas = [1]
-    for i in range(n):
-        omegas.append(omegas[i] * omega % p)
-    for i in range(n):
-        assert omegas[n - i] * omegas[i] % p == 1
-    omegas = omegas[:n]  # Drop the last, needless value.
-
-    return omegas
-
-
-def reverse_bits(number, bit_length):
-    # Reverses the bits of `number` up to `bit_length`.
-    reversed = 0
-    for i in range(0, bit_length):
-        if (number >> i) & 1: reversed |= 1 << (bit_length - 1 - i)
-    return reversed
 
 
 def run_ntt(n, dump_input, dump_output, indata=None):
